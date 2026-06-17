322be90527c9be3e67863f686ef74a651a599d1a
mnstr0 <mnstr0@proton.me>
2026-06-16 23:37:47 -0700

    Initial rand pos move

--- scripts/tama.gd
+++ scripts/tama.gd
@@ -6,8 +6,11 @@ var path_goal_ratio: float = 0.0
 var rand_ratio_mod: int = 0
 var idle_time: float = 0.0
 var current_progress_ratio: float = 0.0
+var moving_right: bool = true
+
 
 signal idle_state_entered
+signal wander_state_entered
 signal moving_state_entered
 
 # enum State {STATE_IDLE, STATE_JUMP = 5, STATE_SHOOT}
@@ -29,6 +32,7 @@ signal moving_state_entered
 
 enum MovementState {
 	IDLE,
+	WANDER,
 	MOVING,
 }
 
@@ -37,7 +41,6 @@ var movement_state: int = MovementState.IDLE
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize() ## This method is called automatically when the project is run
-	randomize_ratio_mod()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -45,6 +48,12 @@ func _process(_delta: float) -> void:
 	if movement_state == MovementState.IDLE:	
 		if tama_sprite.animation != "gen_1_idle":
 			tama_sprite.animation = "gen_1_idle"
+		pass
+	elif movement_state == MovementState.WANDER:
+		if current_progress_ratio != tama_path.progress_ratio:
+			proceed_to_path_goal(current_progress_ratio)
+		if tama_sprite.animation != "gen_1_walk":
+			tama_sprite.animation = "gen_1_walk"
 		pass
 
 func change_state(new_state: int) -> void:
@@ -55,17 +64,22 @@ func randomize_ratio_mod() -> void:
 
 func proceed_to_path_goal(ratio: float) -> void:
 	if tama_path.progress_ratio < ratio:
-		tama_path.progress_ratio += 0.01
+		tama_path.progress_ratio += 0.001
 	elif tama_path.progress_ratio > ratio:
-		tama_path.progress_ratio -= 0.01
-	else:
+		tama_path.progress_ratio -= 0.001
+	elif tama_path.progress_ratio == ratio:
 		pass
 
 func update_current_progress_ratio() -> void:
-	current_progress_ratio = tama_path.progress_ratio
+	current_progress_ratio = tama_path.progress_ratio + rand_ratio_mod
+	if current_progress_ratio > 100:
+		current_progress_ratio -= 100
+	current_progress_ratio /= 100
 
 func _on_idle_timer_timeout() -> void:
 	print("Idle Timer Timedout")
-	change_state(1)
-	emit_signal("moving_state_entered")
+	randomize_ratio_mod()
+	update_current_progress_ratio()
+	change_state(MovementState.WANDER)
+	emit_signal("wander_state_entered")
 	pass # Replace with function body.
