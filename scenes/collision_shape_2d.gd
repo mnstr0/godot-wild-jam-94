08a44227be9c4a55f7731e6413701b8fef51316e
mnstr0 <mnstr0@proton.me>
2026-06-19 21:03:05 -0700

    Food disappears when left_clicked.

---
+++ scenes/collision_shape_2d.gd
@@ -0,0 +1,16 @@
+extends CollisionShape2D
+
+@onready var rigid_body: RigidBody2D = $"../FoodRigidBody"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func despawn_food() -> void:
+	rigid_body.queue_free()
--- scripts/food_rigidbody.gd
+++ scripts/food_rigidbody.gd
@@ -1,10 +1,17 @@
 extends RigidBody2D
 
-
+@onready var collider: CollisionShape2D = $CollisionShape2D
+var mouse_pos: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("left_click"):
+		mouse_pos = get_global_mouse_position()
+		if collider.shape.get_rect().has_point(to_local(mouse_pos)):
+			despawn_food()
 
 
 func despawn_food() -> void:
 	queue_free()
+
